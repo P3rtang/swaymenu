49635f21a7f9fd198e62f0d893a8b68fab693ae6
p3rtang <tomverhulst21@gmail.com>
2022-10-15 00:17:41 +0200

    initial trial and error of rust with gtk4

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use gtk::prelude::*;
+use gtk::{Application, ApplicationWindow, Button};
+use gtk::ffi::gtk_widget_get_style_context;
+
+const APP_ID: &str = "org.gtk_rs.HelloWorld2";
+
 fn main() {
-    println!("Hello, world!");
+    // Create a new application
+    let app = Application::builder().application_id(APP_ID).build();
+
+    // Connect to "activate" signal of `app`
+    app.connect_activate(build_ui);
+
+    // Run the application
+    app.run();
+}
+
+fn build_ui(app: &Application) {
+    let button = Button::builder()
+        .label("Exit Sway")
+        .margin(10)
+        .build();
+
+    let window = ApplicationWindow::builder()
+        .application(app)
+        .title("My GTK App")
+        .child(&button)
+        .build();
+
+
+    // Present window
+    window.present();
 }
