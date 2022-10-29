1813480da09fb3519e215c51d277bb82c25ac024
p3rtang <tomverhulst21@gmail.com>
2022-10-29 20:06:05 +0200

    fixed times being off by a factor of 10 and added sleep lock when unlocked

--- scripts/swayidle.sh
+++ scripts/swayidle.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
-TIME_MOD_SMALL=30
-TIME_MOD_BIG=60
-swayidle -w timeout $TIME_MOD_SMALL 'swaylock -f -i $HOME/Pictures/wallpapers/factorio.jpg && swaymsg "output * dpms off"' resume 'swaymsg "output * dpms on"' timeout $TIME_MOD_BIG 'systemctl suspend' before-sleep 'swaylock -f -i $HOME/Pictures/wallpapers/factorio.jpg'
+TIME_MOD_SMALL=300
+TIME_MOD_BIG=600
+while getopts 'ed' OPTION; do
+  case "$OPTION" in
+    e)
+      swayidle -w timeout $TIME_MOD_SMALL 'swaylock -f -i $HOME/Pictures/wallpapers/factorio.jpg && swaymsg "output * dpms off"' resume 'swaymsg "output * dpms on"' timeout $TIME_MOD_BIG 'systemctl suspend' before-sleep 'swaylock -f -i $HOME/Pictures/wallpapers/factorio.jpg'
+      ;;
+    d)
+      killall swayidle
+      swayidle -w before-sleep 'swaylock -f -i $HOME/Pictures/wallpapers/factorio.jpg'
+      ;;
+  esac
+done
+shift "$(($OPTIND -1))"
